0010f18ceeb257cab5d6ad7adef36aec1e8d00c4
Akshat Jaipuria <38358340+akshatjaipuria@users.noreply.github.com>
2019-10-02 09:46:58 +0530

    Update Floyd.java

--- Floyd.java
+++ Floyd.java
@@ -22,6 +22,7 @@ public class Floyd
 	}
 	public static void floyd(int n)
 	{
+		//simply implementing 3 nested loops and checking the combination of paths as described by Floyd's algorithm
 		for(int k=1;k<=n;k++)
 			for(int i=1;i<=n;i++)
 				for(int j=1;j<=n;j++)
